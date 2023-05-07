67358d46c3240ecfb5d972fefa15bd148583efed
kosaksongul <kosaksongul@gmail.com>
2023-05-07 23:12:15 +0300

    7 mayıs

---
+++ Collecions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] isimler = new string[] { "Engin", "Murat","Kerem","Halil" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];
+            //isimler[4] = "İlker";
+            //Console.WriteLine(isimler[4]);
+
+
+            List<string> isimler2 = new List<string>();
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
